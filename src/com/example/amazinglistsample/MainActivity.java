0bcb3d8dfdcdb792a2e9a64b9a5c0e5a2fde7f26
Vishnu ravi <vishnu.r@mobme.in>
2013-08-22 18:21:20 +0530

    Adding section header to listview: criteria is group by date

--- src/com/example/amazinglistsample/MainActivity.java
+++ src/com/example/amazinglistsample/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.amazinglistsample;
 
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
 
@@ -15,9 +16,11 @@ import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
 import android.support.v4.widget.CursorAdapter;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -93,7 +96,8 @@ public class MainActivity extends FragmentActivity implements LoaderManager.Load
     }
 
     /**
-     * Custom sectioned Adapter
+     * Custom sectioned Adapter.
+     * Source: http://kmansoft.com/2010/11/16/adding-group-headers-to-listview/ 
      */
     class SectionedCursorAdapter extends CursorAdapter {
         private static final int VIEW_TYPE_GROUP_START = 0;
@@ -102,6 +106,9 @@ public class MainActivity extends FragmentActivity implements LoaderManager.Load
 
         private LayoutInflater mInflater;
 
+        public int mColSummary = -1;
+        public int mColCreatedAt = -1;
+
         private final SimpleDateFormat gDateFormatDataItem = new SimpleDateFormat("E, dd MMM yyyy HH:mm:ss", Locale.US);
 
         public SectionedCursorAdapter(Context context, Cursor cursor, boolean autoRequery) {
@@ -109,40 +116,128 @@ public class MainActivity extends FragmentActivity implements LoaderManager.Load
             mInflater = LayoutInflater.from(context);
         }
 
-        // @Override
-        // public int getViewTypeCount() {
-        // return VIEW_TYPE_COUNT;
-        // }
+        @Override
+        public int getViewTypeCount() {
+            return VIEW_TYPE_COUNT;
+        }
+
+        @Override
+        public int getItemViewType(int position) {
+            // There is always a group header for the first data item
+            if (position == 0) {
+                return VIEW_TYPE_GROUP_START;
+            }
+
+            // For other items, decide based on current data
+            Cursor cursor = getCursor();
+            cursor.moveToPosition(position);
+            boolean newGroup = isNewGroup(cursor, position);
+
+            // Check item grouping
+            if (newGroup) {
+                return VIEW_TYPE_GROUP_START;
+            } else {
+                return VIEW_TYPE_GROUP_CONT;
+            }
+        }
 
         @Override
         public void bindView(View view, Context context, Cursor cursor) {
             ViewHolder holder = (ViewHolder) view.getTag();
-            holder.itemLabel.setText(cursor.getString(holder.mColSummary));
-            Date date = new Date(cursor.getLong(holder.mColCreatedAt));
-            holder.itemDate.setText(gDateFormatDataItem.format(date));
+            holder.itemLabel.setText(cursor.getString(mColSummary));
+            holder.itemDate.setText("dummy data");
+
+            if (holder.itemHeader.getVisibility() == View.VISIBLE) {
+                Date date = new Date(cursor.getLong(mColCreatedAt));
+                holder.itemHeader.setText(gDateFormatDataItem.format(date));
+            }
+
         }
 
         @Override
         public View newView(Context context, Cursor cursor, ViewGroup parent) {
-            View rowView = mInflater.inflate(R.layout.todo_row, parent, false);
+            if (mColSummary == -1) {
+                mColSummary = cursor.getColumnIndexOrThrow(TodoTable.COLUMN_SUMMARY);
+            }
+            if (mColCreatedAt == -1) {
+                mColCreatedAt = cursor.getColumnIndexOrThrow(TodoTable.COLUMN_NAME_CREATE_DATE);
+            }
+
+            int position = cursor.getPosition();
+            int nViewType;
+
+            if (position == 0) {
+                // Group header for position 0
+                nViewType = VIEW_TYPE_GROUP_START;
+            } else {
+                // For other positions, decide based on data
+                boolean newGroup = isNewGroup(cursor, position);
+
+                if (newGroup) {
+                    nViewType = VIEW_TYPE_GROUP_START;
+                } else {
+                    nViewType = VIEW_TYPE_GROUP_CONT;
+                }
+            }
+
+            View rowView = mInflater.inflate(R.layout.item, parent, false);
+            View header = rowView.findViewById(R.id.todo_header);
+
+            if (nViewType == VIEW_TYPE_GROUP_START) {
+                // Ignore clicks on the list header
+                header.setVisibility(View.VISIBLE);
+                header.setOnClickListener(new OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        // nop
+                    }
+                });
+            } else {
+                header.setVisibility(View.GONE);
+            }
 
             ViewHolder holder = new ViewHolder();
-            holder.mColSummary = cursor.getColumnIndexOrThrow(TodoTable.COLUMN_SUMMARY);
-            holder.mColCreatedAt = cursor.getColumnIndexOrThrow(TodoTable.COLUMN_NAME_CREATE_DATE);
-            holder.itemLabel = (TextView) rowView.findViewById(R.id.todo_label);
+            holder.itemLabel = (TextView) rowView.findViewById(R.id.todo_item);
             holder.itemDate = (TextView) rowView.findViewById(R.id.todo_date);
+            holder.itemHeader = (TextView) header;
             rowView.setTag(holder);
 
             return rowView;
         }
 
+        private boolean isNewGroup(Cursor cursor, int position) {
+            // Get date values for current and previous data items
+            long nWhenThis = cursor.getLong(mColCreatedAt);
+
+            cursor.moveToPosition(position - 1);
+            long nWhenPrev = cursor.getLong(mColCreatedAt);
+
+            // Restore cursor position
+            cursor.moveToPosition(position);
+
+            // Compare date values, ignore time values
+            Calendar calThis = Calendar.getInstance();
+            calThis.setTimeInMillis(nWhenThis);
+
+            Calendar calPrev = Calendar.getInstance();
+            calPrev.setTimeInMillis(nWhenPrev);
+
+            int nDayThis = calThis.get(Calendar.DAY_OF_YEAR);
+            int nDayPrev = calPrev.get(Calendar.DAY_OF_YEAR);
+
+            if (nDayThis != nDayPrev || calThis.get(Calendar.YEAR) != calPrev.get(Calendar.YEAR)) {
+                return true;
+            }
+
+            return false;
+        }
+
     }
 
     static class ViewHolder {
-        public int mColSummary;
-        public int mColCreatedAt;
         public TextView itemLabel;
         public TextView itemDate;
+        public TextView itemHeader;
     }
 
 }
